284f4a1bdb58e3066e9a96705691bbc3f76a0835
Marifer Muñoz <marifer.2228@gmail.com>
2017-08-04 12:09:04 -0600

    new files install

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // generated on 2017-07-03 using generator-webapp 3.0.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
+const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
 const del = require('del');
 const wiredep = require('wiredep').stream;
@@ -10,6 +11,15 @@ const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
 let dev = true;
+
+gulp.task('fileinclude', function() {
+  gulp.src(['app/*.html'])
+    .pipe(fileinclude({
+      indent: true
+    }))
+    .pipe(gulp.dest('.tmp'))
+    .pipe(reload({stream: true}));
+});
 
 gulp.task('styles', () => {
   return gulp.src('app/styles/*.scss')
@@ -53,8 +63,8 @@ gulp.task('lint:test', () => {
     .pipe(gulp.dest('test/spec'));
 });
 
-gulp.task('html', ['styles', 'scripts'], () => {
-  return gulp.src('app/*.html')
+gulp.task('html', ['styles', 'scripts', 'fileinclude'], () => {
+  return gulp.src('.tmp/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
     .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
@@ -95,7 +105,7 @@ gulp.task('extras', () => {
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 gulp.task('serve', () => {
-  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
+  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts', 'fileinclude'], () => {
     browserSync.init({
       notify: false,
       port: 9000,
@@ -108,11 +118,12 @@ gulp.task('serve', () => {
     });
 
     gulp.watch([
-      'app/*.html',
+      '.tmp/*.html',
       'app/images/**/*',
       '.tmp/fonts/**/*'
     ]).on('change', reload);
 
+    gulp.watch(['app/*.html', 'app/templates/*.html'], ['fileinclude']);
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
     gulp.watch('app/fonts/**/*', ['fonts']);
