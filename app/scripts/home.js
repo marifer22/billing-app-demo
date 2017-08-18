c73e5ad5b6fee4706a0019edb37c6026182b09b9
Marifer Muñoz <marifer.2228@gmail.com>
2017-08-17 20:35:34 -0600

    make the js

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -9,15 +9,20 @@
      * jQuery/DOM code here
      */
     init: function() {
-      // Example on how to call a function
-      LI.pages.home.greet();
+      LI.pages.home.fetchBooks();
     },
 
     /**
      * Greet function to use as a test
      */
-    greet: function() {
-      console.log('Hello from the home page');
+    fetchBooks: function() {
+      LI.global.fetchBooks(function(books) {
+        for (var book = 0; book < books.length; book++){
+          $('.main-content').append(
+            '<div class="col-sm-4"> <div class="row product-box"> <div class="col-sm-4 product-image"> <a href="#"> <img src="'+books[book].image+'" class="img-responsive"> </a> </div> <div class="col-sm-8 product-info"> <ul class="list-unstyled"> <li><a href="#">'+books[book].title+'</li> <li><a href="#">'+books[book].author.name+'</a></li> <li><a href="#">'+books[book].category.name+'</a></li> <li><a href="#">'+books[book].publisher.name+'</a></li> <li>'+books[book].year+'</li> <li> ₡'+books[book].price+'</li> </ul> </div> </div> </div>'
+          )
+        }
+      });
     }
   };
 })(window.LI, jQuery);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ window.LI = {
    * LI.global namespace, for global logic avaialbe in the whole app
    */
   LI.global = {
-    baseApi: 'http://localhost:3001',
+    baseApi: 'http://localhost:3002',
 
     /**
      * jQuery/DOM code here
@@ -21,8 +21,20 @@ window.LI = {
       for (var page in LI.pages) {
         LI.pages[page].init();
       }
+    },
+
+    fetchBooks: function(callback) {
+      $.ajax(LI.global.baseApi + '/books?_expand=author&_expand=category&_expand=publisher&&_limit=12')
+        .done(function (data) {
+          console.log(data);
+
+          if (callback) {
+            callback(data);
+          }
+        });
     }
   };
 
   $(LI.global.init);
 })(window.LI, jQuery);
+
