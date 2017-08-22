5acef37e0923a91d974a8a3e41a80358cbebea31
Marifer Muñoz <marifer.2228@gmail.com>
2017-08-21 21:27:11 -0600

    change image for thumb

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -19,7 +19,7 @@
       LI.global.fetchBooks(function(books) {
         for (var book = 0; book < books.length; book++){
           $('.main-content').append(
-            '<div class="col-sm-4"> <div class="row product-box"> <div class="col-sm-4 product-image"> <a href="#"> <img src="'+books[book].image+'" class="img-responsive"> </a> </div> <div class="col-sm-8 product-info"> <ul class="list-unstyled"> <li><a href="#">'+books[book].title+'</li> <li><a href="#">'+books[book].author.name+'</a></li> <li><a href="#">'+books[book].category.name+'</a></li> <li><a href="#">'+books[book].publisher.name+'</a></li> <li>'+books[book].year+'</li> <li> ₡'+books[book].price+'</li> </ul> </div> </div> </div>'
+            '<div class="col-sm-4"> <div class="row product-box"> <div class="col-sm-4 product-image"> <a href="#"> <img src="'+books[book].thumb+'" class="img-responsive"> </a> </div> <div class="col-sm-8 product-info"> <ul class="list-unstyled"> <li><a href="#">'+books[book].title+'</li> <li><a href="#">'+books[book].author.name+'</a></li> <li><a href="#">'+books[book].category.name+'</a></li> <li><a href="#">'+books[book].publisher.name+'</a></li> <li>'+books[book].year+'</li> <li> ₡'+books[book].price+'</li> </ul> </div> </div> </div>'
           )
         }
       });
